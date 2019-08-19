9589020912914350bbe71a608e3df2fcff9d6356
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-08-19 21:52:16 +0530

    print the array

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
-void leftRotate(int arr[],int d,int n)
+void leftRotatebyOne(int a[],int n);
+void leftRotate(int a[],int m,int n)
 {
   int i;
-  for(i=0;i<d;i++)
-      leftRotatebyOne(arr,n);
+  for(i=0;i<m;i++)
+      leftRotatebyOne(a,n);
 }
-void leftRotatebyOne(int arr[],int n)
+void leftRotatebyOne(int a[],int n)
+{
+   int temp=a[0],i;
+   for(i=0;i<n-1;i++)
+       a[i]=a[i+1];
+   a[i]=temp;
+}
+void print(int a[],int n)
 {
 
 }
