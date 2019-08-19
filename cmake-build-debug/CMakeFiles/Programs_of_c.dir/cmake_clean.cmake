137f9a4e21518bb0f7872197b2b9f5aa732c0217
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-08-19 21:45:46 +0530

    init commit

---
+++ cmake-build-debug/CMakeFiles/Programs_of_c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Programs_of_c.dir/main.c.obj"
+  "Programs_of_c.pdb"
+  "Programs_of_c.exe"
+  "Programs_of_c.exe.manifest"
+  "libPrograms_of_c.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Programs_of_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void leftRotate(int arr[],int d,int n)
+{
+
+}
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
