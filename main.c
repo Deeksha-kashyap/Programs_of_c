d30aeb3a68cce921cfb12d838ecf5790cb796d80
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-08-19 21:48:30 +0530

    function to left rotate the array

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 void leftRotate(int arr[],int d,int n)
 {
+  int i;
+  for(i=0;i<d;i++)
+      leftRotatebyOne(arr,n);
+}
+void leftRotatebyOne(int arr[],int n)
+{
 
 }
 int main() {
