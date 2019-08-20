a2ef92a3123d937812485f80f55912accb117c00
Deeksha Kashyap <Deeksha Kashyap@DESKTOP-KR9UPQB>
2019-08-20 21:29:05 +0530

    take the input of the element

--- arrayrotation.c
+++ arrayrotation.c
@@ -4,7 +4,12 @@
 #include<stdio.h>
 int main()
 {
-    int i,n;
+    int i,n,arr[10];
     printf("Enter the size of the array");
     scanf("%d",&n);
+    printf("Enter the element of array");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
 }
